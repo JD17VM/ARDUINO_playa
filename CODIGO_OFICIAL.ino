76d20023dc07c55d763e3a189abbc8b5af80a886
KodingT <kodingtutoriales@gmail.com>
2022-08-28 17:15:40 -0500

    OTRO

--- CODIGO_OFICIAL.ino
+++ CODIGO_OFICIAL.ino
@@ -1,8 +1,7 @@
 #include <LiquidCrystal_I2C.h>
-
 #include <Servo.h>
 
-//--------DEFINIR CONSTANTES Y PINES--------
+//--------DEFINIR CONSTANTES Y PINES---------------------
 
 const short int TRIG_ENTRADA = 4;    // trigger en pin 4
 const short int ECO_ENTRADA = 3;      // echo en pin 3
@@ -13,16 +12,13 @@ const short int ECO_SALIDA = 6;      // echo en pin 3
 const short int SERVO_ENTRADA = 9;
 const short int SERVO_SALIDA = 11;
 
-//-------------------------------------------
-
+//---------------CREAR OBJETOS----------------------------
 
 Servo myServo_entrada;
 Servo myServo_salida;
-
-short int espacios_totales = 9;
-short int espacios_ocupados = 0;
-
 LiquidCrystal_I2C lcd(0x27,16,2);
+
+//--------------------------------------------------------
 
 int distancia_medida_cm(int trig,int eco,String tipo)
 {
@@ -37,13 +33,15 @@ int distancia_medida_cm(int trig,int eco,String tipo)
     return d;
 }
 
+short int espacios_totales = 9;
+short int espacios_ocupados = 0;
 bool bandera_entrada = false;
 bool bandera_salida = false;
 int contador_entrada = 0;
 int contador_salida = 0;
+
 void calcular_distancia_prender_led_servo(int d,int servo,class Servo &servomotor,bool &bandera, int &contador)
 {
-    
     if (d <= 110 && d >= 0){ //VARIA      
         servomotor.write(0);
         bandera = true;
@@ -55,17 +53,7 @@ void calcular_distancia_prender_led_servo(int d,int servo,class Servo &servomoto
         contador++;
       }
       delay(2000);
-    }
-
-    
-    
-}
-
-void iniciar_LCD(){
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-  lcd.display();
+    } 
 }
 
 void mostrar_texto_LCD(int espacio_disponible){
@@ -82,11 +70,16 @@ void mostrar_texto_LCD(int espacio_disponible){
     lcd.setCursor(0,0);
     lcd.print(">NO HAY ESPACIO<");
   }
-  
 }
 
-
 //------------------------------------------------------------------------------------------------
+
+void iniciar_LCD(){
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.display();
+}
 
 void setup()
 {
@@ -102,12 +95,10 @@ void setup()
     iniciar_LCD();
   
     Serial.begin(9600);     // inicializacion de comunicacion serial a 9600 bps
-
 }
 
 void loop()
 {
-
     delay(500);
     short int distancia_medida_entrada = distancia_medida_cm(TRIG_ENTRADA,ECO_ENTRADA,"entrada");//delay(200);
     short int distancia_medida_salida = distancia_medida_cm(TRIG_SALIDA,ECO_SALIDA,"salida");//delay(200);
@@ -119,7 +110,6 @@ void loop()
     calcular_distancia_prender_led_servo(distancia_medida_salida,SERVO_SALIDA,myServo_salida, bandera_salida, contador_salida);
 
     espacios_ocupados = contador_entrada - contador_salida;
-  
-  
+
     mostrar_texto_LCD(espacios_totales - espacios_ocupados);
 }
